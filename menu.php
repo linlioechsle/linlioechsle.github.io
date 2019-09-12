81a85ea15ebfda1a95abf4033438bea176756dfd
linlioechsle <46604675+linlioechsle@users.noreply.github.com>
2019-09-12 00:24:09 -0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,11 +1,11 @@
 <?php
 $pages = scandir('pages');
-$notWanted = array('.', '..', 'index.php');
+$notWanted = array('.', '..', 'index.html');
 echo '<div class="menu">';
 foreach($pages as $page){
     if(!in_array($page, $notWanted){
     $link = 'pages/' . $page;
-    $safeName = strtoupper(str_replace('.php', '', $page));
+    $safeName = strtoupper(str_replace('.html', '', $page));
         echo'<a href="' . $link . '">' . $safeName . '</a> &nbsp; &nbsp; &nbsp; &nbsp;';
     } // close if not in array
 } // close foreach
