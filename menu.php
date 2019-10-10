35edd2750c84fddbc3fd6a4de7e827b0756cd949
linlioechsle <46604675+linlioechsle@users.noreply.github.com>
2019-10-10 18:31:19 -0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -11,3 +11,28 @@ foreach($pages as $page){
 } // close foreach
 echo '</div>';
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,height=device-height,initial-scale=1">
+
+<title>untitled document</title>
+
+<link rel="stylesheet" href="css/screen.css" media="screen">
+
+</head>
+<body> 
+<h1>Page Title</h1>
+<?php
+   $menu=file_get_contents("index.html");
+   $base=basename($_SERVER['PHP_SELF']);
+   $menu=preg_replace("|<li><a href=\"".$base."\">(.*)</a></li>|U", "<li id=\"current\">$1</li>", $menu);
+   echo $menu;
+?>
+
+</body>
+</html>
